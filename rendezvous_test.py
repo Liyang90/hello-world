8a04fe432c43677c7889860a4b4fa88ce1776965
Liyang90 <lu.liyang.1990@gmail.com>
2023-02-06 14:23:33 -0800

    Create rendezvous_test.py

---
+++ rendezvous_test.py
@@ -0,0 +1,24 @@
+import time
+import torch_xla.core.xla_model as xm
+import torch_xla.debug.profiler as xp
+import torch_xla.distributed.xla_multiprocessing as xmp
+from torch_xla.experimental import pjrt
+
+def fn(index):
+  device = xm.xla_device()
+  if pjrt.using_pjrt():
+    print(pjrt.device_attributes(str(device)), f"rank {pjrt.global_ordinal()}")
+
+  timing = []
+  for i in range(1):
+    t_start = time.perf_counter()
+    xm.rendezvous(f"test_{i}")
+    t_end = time.perf_counter()
+    timing.append(t_end - t_start)
+
+  print(f"rank {xm.get_ordinal()}", timing)
+
+
+if __name__ == '__main__':
+
+  xmp.spawn(fn, args=(), nprocs=4)
